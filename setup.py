3bd93cbdf9acda042c460d6f17e5dcffe7c2e9ed
Konrad Sakowski <22024843+ghkonrad@users.noreply.github.com>
2021-09-22 15:02:18 +0200

    Version 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="ksiterate",
-	version="1.0",
+	version="1.1",
 	author="Konrad Sakowski",
 	description="Module for iteration over product of given parameter values",
 	long_description=long_description,
